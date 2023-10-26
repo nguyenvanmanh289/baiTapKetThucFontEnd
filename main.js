d580e4abe4a8038cc42017c9f02b4446599f03be
my_autoFill <manhnguyenbg2003@gmail.com>
2023-10-26 11:41:33 +0700

    only commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const navSubmit = document.getElementsByClassName('form-control')[0];
+navSubmit.addEventListener('change',(e)=>{
+    alert('no search engine')
+})
+
+//find house
+const findSubmit = document.querySelector("#others input");
+findSubmit.addEventListener('click',(e)=>{
+    
+    alert('no serch engine')
+})
